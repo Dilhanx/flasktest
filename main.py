8217dc0caf64e6cdeff4caf2f7963500d40a388e
Dilhanx <dilhanx@outlook.com>
2017-07-27 23:54:40 +0530

    Signed-off-by: Dilhanx <dilhanx@outlook.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 import requests
+import request
 import pyodbc
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def hello_world():
 
 
 
-@app.route("/login")
+@app.route("/login",methods=['POST'])
 def login():
     print("Start")
     file = open("D:/New folder (26)/dbc.txt", "r")
@@ -32,22 +33,16 @@ def login():
     print("File close")
 
     #Driver={ODBC Driver 13 for SQL Server};Server=tcp:kappaorbanned.database.windows.net,1433;Database=TwitchStats;Uid=Dilhan@kappaorbanned;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
-    db =  pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+    db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
     print("DB Set")
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-    cursor.execute("SELECT * From User_Account ")
+    cursor.execute("SELECT count(*) From user_account where username="+request.form['username']+" and password ="+request.form['password']+";")
     row = cursor.fetchone()
-    x=0
-    sql_list =[]
-    while row:
-        print (row.username +" " +row.email+ " "+row.password)
-        data=(row.username , row.email,row.password)
-        x+=1
-        sql_list.append(data)
-        row = cursor.fetchone()
-    j = json.dumps( sql_list)
-    return j
+    if row.count==1:
+      return {'status': 'login'}
+    else:
+      return {'status': 'incorrect'}
 
 
 
