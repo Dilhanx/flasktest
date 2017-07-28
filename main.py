3289cfa65ed3bbf7a2dd877e23a00b9545080734
Dilhanx <dilhanx@outlook.com>
2017-07-28 13:44:20 +0530

    Signed-off-by: Dilhanx <dilhanx@outlook.com>

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 import requests
+import pyodbc 
 
-import pyodbc
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
 
+@app.route('/test',methods=['POST'])
+def test():
+  account=[]
+ 
+  data=("username : "+request.form['username'],"password : "+request.form['password'])
+  print (data)
+  account.append(data)
+  j = json.dumps(data)
+  print (j)
+  return j
 
+@app.route('/ain')
+def ain():
+  return render_template('main.html')
 
 @app.route('/login',methods=['POST'])
 def login():
@@ -20,29 +33,32 @@ def login():
     # Asign values to  send
 
     # Asign database connection details
-    server = text[0]
+    server = "kappaorbannedj.database.windows.net"
 
-    database = text[1]
+    database = "TwitchStats"
 
-    username = text[2]
+    username = "Dilhan@kappaorbannedj"
 
-    password = text[3]
+    password = "101Luminous101"
    
-    driver=text[4]    
+    driver="{ODBC Driver 13 for SQL Server}" 
     file.close()
     print("File close")
 
     #Driver={ODBC Driver 13 for SQL Server};Server=tcp:kappaorbanned.database.windows.net,1433;Database=TwitchStats;Uid=Dilhan@kappaorbanned;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+    #db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
     db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
     print("DB Set")
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-    cursor.execute("SELECT count(*) From user_account where username="+request.form['username']+" and password ="+request.form['password']+";")
+    print (request.form['username'] +" "+request.form['password'])
+    cursor.execute("SELECT * From user_account where username= '"+request.form['username']+"' AND password ='"+request.form['password']+"' ;")
     row = cursor.fetchone()
-    if row.count==1:
-      return {'status': 'login'}
+    print(row)
+    if row.cou==1:
+      return {"\"status: login\""}
     else:
-      return {'status': 'incorrect'}
+      return {"status: incorrect"}
 
 
 
